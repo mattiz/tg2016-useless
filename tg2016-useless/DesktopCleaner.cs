fcf6cef1f02260be1d5e94bb9713cede925f81cc
Mathias Bjerke <mattiz@online.no>
2016-03-25 02:28:22 +0100

    Initial commit

---
+++ tg2016-useless/DesktopCleaner.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Drawing;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace tg2016_useless
+{
+    class DesktopCleaner
+    {
+        private const int SW_MAXIMIZE = 3;
+        private const int SW_MINIMIZE = 6;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowTextLength(HandleRef hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowText(HandleRef hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+        public DesktopCleaner()
+        {
+            var t = new Thread(() =>
+            {
+
+                while (true)
+                {
+                    Thread.Sleep(5000);
+
+                    IntPtr fw = GetForegroundWindow();
+
+                    String windowTitle = getWindowTitle(fw);
+
+                    Console.WriteLine("Foreground Window: " + windowTitle);
+
+
+                    if (windowTitle.Length > 0)
+                    {
+                        RECT rct = new RECT();
+                        GetWindowRect(fw, ref rct);
+
+                        var p1 = new Point(Cursor.Position.X, Cursor.Position.Y);
+                        var p2 = new Point(rct.Right - 90, rct.Top + 5);
+
+                        animateMouse(p1, p2);
+
+                        Thread.Sleep(500);
+
+                        ShowWindow(fw, SW_MINIMIZE);
+                    }
+                }
+            });
+
+
+            t.Start();
+        }
+
+
+        private String getWindowTitle( IntPtr handle )
+        {
+            int capacity = GetWindowTextLength(new HandleRef(this, handle)) * 2;
+            StringBuilder stringBuilder = new StringBuilder(capacity);
+            GetWindowText(new HandleRef(this, handle), stringBuilder, stringBuilder.Capacity);
+
+            return stringBuilder.ToString();
+        }
+
+        private void animateMouse( Point p1, Point p2 )
+        {
+            for (double i = 0; i < 100; i++)
+            {
+                double blend = i / 100;
+
+                double x = p1.X + blend * (p2.X - p1.X);
+                double y = p1.Y + blend * (p2.Y - p1.Y);
+
+                int ix = (int)x;
+                int iy = (int)y;
+
+                Cursor.Position = new Point(ix, iy);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
---
+++ tg2016-useless/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Media;
+
+namespace tg2016_useless
+{
+    public class SysTrayApp : Form
+    {
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+        private DesktopCleaner desktopCleaner;
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new SysTrayApp());
+        }
+
+
+        public SysTrayApp()
+        {
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("About", OnAbout);
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "Desktop Cleaner";
+            trayIcon.Icon = tg2016_useless.Properties.Resources.star_black;
+
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+
+            desktopCleaner = new DesktopCleaner();
+        }
+
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+
+            base.OnLoad(e);
+        }
+
+
+        private void OnAbout(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desktop Cleaner - TG 2016 Useless contribution by #MooG");
+        }
+
+
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                trayIcon.Dispose();
+            }
+
+            base.Dispose(isDisposing);
+        }
+    }
+}
